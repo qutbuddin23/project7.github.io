495e401884a6ce077c16e875253ab742d995071c
qutbuddin23 <68780372+qutbuddin23@users.noreply.github.com>
2023-05-28 15:15:05 +0530

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WYCYL8DJ6W"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WYCYL8DJ6W');
+</script>
